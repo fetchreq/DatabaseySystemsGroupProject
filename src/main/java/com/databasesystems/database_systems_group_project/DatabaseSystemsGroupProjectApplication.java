0fad62727bb13c898c6cc89c7e280f8e8e265db6
Ryan <ryan-price@uiowa.edu>
2019-03-26 17:04:04 -0500

    first commit

---
+++ src/main/java/com/databasesystems/database_systems_group_project/DatabaseSystemsGroupProjectApplication.java
@@ -0,0 +1,13 @@
+package com.databasesystems.database_systems_group_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseSystemsGroupProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabaseSystemsGroupProjectApplication.class, args);
+    }
+
+}
